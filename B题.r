a6090996e33a4d058558f4ab9b596a020c4f2be4
qwertycxz <gmail2@qq.com>
2023-09-08 14:49:55 +0800

    第二问完成

--- B题.r
+++ B题.r
@@ -1,9 +1,11 @@
+library(measurements)
+
 # 第一问
 depth_delta <- tan(pi / 120) * 200
 raw_result1 <- read.csv("result1.csv", fileEncoding = "UTF-8-BOM", row.names = 1)
 raw_result1$X.800 <- c(NA, NA, 0)
 raw_result1[1, ] <- 4:-4 * depth_delta + 70
-raw_result1[2, ] <- raw_result1[1, ] * (sin(pi * 5 / 6) * tan(pi / 3) / sin(pi * 19 / 120) + sin(pi / 3) / sin(pi * 21 / 120))
+raw_result1[2, ] <- raw_result1[1, ] * (sinpi(5 / 6) * tan(pi / 3) / sinpi(19 / 120) + sin(pi / 3) / sinpi(7 / 40))
 raw_result1[3, ] <- 1 - 200 / raw_result1[2, ]
 write.csv(raw_result1, "result1_raw.csv")
 raw_result1[2, ] <- raw_result1[2, ] * cos(pi / 120)
@@ -19,11 +21,18 @@ lines(c(-1000, 1000), c(0, 0), lty = "dashed")
 polygon(c(-1000, 1000, 1000, -1000), c(-(depth_delta * 5 + 70), depth_delta * 5 - 70, -100, -100), col = rgb(1, 1, 1, 0.75))
 
 
+# 第二问
 raw_result2 <- read.csv("result2.csv", fileEncoding = "UTF-8-BOM")
 colnames(raw_result2) <- raw_result2[1, ]
-raw_result2 <- raw_result2[-1, ]
-row.names(raw_result2) <- raw_result2[, 2]
-raw_result2 <- raw_result2[, -(1:2)]
+named_result2 <- raw_result2[-1, ]
+row.names(named_result2) <- named_result2[, 2]
+matrix_result2 <- named_result2[, -(1:2)]
+depth_cos <- cospi(as.integer(row.names(matrix_result2)) / 180)
+depth_angle <- atan(depth_cos * tan(pi / 120))
+cover_result2 <- (depth_cos %*% t(conv_unit(as.double(colnames(matrix_result2)), "naut_mi", "m")) * tan(pi / 120) + 120) * (sinpi(5 / 6) * tan(pi / 3) / sin(pi / 6 - depth_angle) + sin(pi / 3) / sinpi(pi / 6 + depth_angle))
+write.csv(cover_result2, "result2_raw.csv")
+
+
 raw_attachment <- read.csv("附件.csv", fileEncoding = "UTF-8-BOM")
 colnames(raw_attachment) <- raw_attachment[1, ]
 raw_attachment <- raw_attachment[-1, ]
